aeca2189d87aada2dfec7e01d97b8e4466edbcf3
John J Kavanagh <john.kavanagh@kavanaghtech.net>
2016-04-30 08:59:11 -0400

    clear-edgefavorites
    :information_source:  Clear edge favorties

---
+++ clear-edgefavorites.ps1
@@ -0,0 +1,10 @@
+﻿<#
+    Below is DOS command
+DEL /F /S /Q /A "%LocalAppData%\Packages\Microsoft.MicrosoftEdge_8wekyb3d8bbwe\AC\MicrosoftEdge\User\Default\DataStore"
+#>
+remove-item -Path "c:\users\johnk\appdata\local\Packages\Microsoft.MicrosoftEdge_8wekyb3d8bbwe\AC\MicrosoftEdge\User\Default\DataStore" -Recurse -force
+<#
+Below is DOS Command
+RD /S /Q "%LocalAppData%\Packages\Microsoft.MicrosoftEdge_8wekyb3d8bbwe\AC\MicrosoftEdge\User\Default\Favorites"
+#>
+ remove-item -Path "c:\users\johnk\appdata\local\Packages\Microsoft.MicrosoftEdge_8wekyb3d8bbwe\AC\MicrosoftEdge\User\Default\Favorites" -Recurse -force

4de7cdecc9fb4b98f00701ef51a576130144f46a
John J Kavanagh <john.kavanagh@kavanaghtech.net>
2016-05-11 11:22:30 -0400

    get-vmmrestarts.ps1
    :dog2:  One-liner used after crash. Tweak into a tool to allow passing
    of eventid or event message

---
+++ Hyper-v/get-vmmrestarts.ps1
@@ -0,0 +1,3 @@
+﻿get-eventlog -LogName System -after (Get-Date).AddDays(-1) | 
+? {$_.EventId -eq 7036 -and $_.Message -like "*Hyper-V Virtual Machine Management service*" } | 
+select MachineName, Timegenerated, Message | fl | clip

3c143582ddfb586b9d7d0f01151cada8c4c74cd9
John J Kavanagh <john.kavanagh@kavanaghtech.net>
2016-07-21 17:26:56 -0400

    Reset-wsusclient
    Script run locally to reset the WSUS Client :newspaper:

--- reset-wsusclient.ps1
+++ reset-wsusclient.ps1
@@ -1,19 +1,32 @@
-stop-service wuauserv, appidsvc,cryptsvc 
-# WSUS Data folder
-If (test-path C:\windows\SoftwareDistribution.old){
-	Remove-item C:\windows\SoftwareDistribution.old -force
-	rename-item C:\windows\SoftwareDistribution -NewName C:\windows\SoftwareDistribution.old
-}
-Else {
-	rename-item C:\windows\SoftwareDistribution -NewName C:\windows\SoftwareDistribution.old
-}
+<# 
+	.SYNOPSIS 
+		Performs steps to reset the WSUS Client. 
+	
+	.DESCRIPTION 
+		Performs steps to reset the WSUS Client.
+	
+	.NOTES
+        07.11.2016 JJK: TODO break into functions as cryptsvc can be problematic but not essential in this process
+	.AUTHOR
+		John J Kavanagh
+ #>
+stop-service wuauserv, appidsvc,cryptsvc -Force -confirm:$False
 # Update catalog
 if (test-path c:\windows\system32\catroot2.bak){
-	Remove-item c:\windows\system32\catroot2.bak -Force
-	Rename-Item "c:\windows\system32\catroot2" -NewName "c:\windows\system32\catroot2.bak" -Force
+	Remove-item c:\windows\system32\catroot2.bak -Force -Confirm:$false -Recurse
+	Rename-Item "c:\windows\system32\catroot2" -NewName "c:\windows\system32\catroot2.bak" -Force -Confirm:$false 
 }
 Else {
-	Rename-Item "c:\windows\system32\catroot2" -NewName "c:\windows\system32\catroot2.bak" -Force
+	Rename-Item "c:\windows\system32\catroot2" -NewName "c:\windows\system32\catroot2.bak" -Force -Confirm:$false
+}
+
+# WSUS Data folder
+If (test-path C:\windows\SoftwareDistribution.old){
+	Remove-item C:\windows\SoftwareDistribution.old -force -Confirm:$false -Recurse
+	rename-item C:\windows\SoftwareDistribution -NewName C:\windows\SoftwareDistribution.old -Force -Confirm:$false
+}
+Else {
+	rename-item C:\windows\SoftwareDistribution -NewName C:\windows\SoftwareDistribution.old -Force -Confirm:$false
 }
 # Start all services back up
 start-service wuauserv, appidsvc,cryptsvc 
